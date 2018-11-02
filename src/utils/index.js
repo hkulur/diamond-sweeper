79b8b7e6df19966a5650a55e2f9190dc496cc36d
Harish <kulur.harish@gmail.com>
2018-11-02 11:27:00 +0530

    changed the randomize board logic

--- src/utils/index.js
+++ src/utils/index.js
@@ -7,12 +7,15 @@ function getRandomInt(max) {
 
 /*generates diamonds at random places given a board size*/
 export function randomizeBoard(size){
-	const board = new Array(size * size).fill(null);
-	for(var i= 0; i< size; i++){
-		const randomIndex = getRandomInt(size);
-		const diamondIndex = (i * size) + randomIndex;
-		board[diamondIndex] = DIAMOND;
+	const board_size = size * size;
+	const board = new Array(board_size).fill(null);
+	var i=0;
+	while(i < size){
+		const randomIndex = getRandomInt(board_size);
+		if(!board[randomIndex]){
+			board[randomIndex] = DIAMOND;
+			i++;
+		}
 	}
 	return board;
-
 }
